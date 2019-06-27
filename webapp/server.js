b45b8f4c3904d4e4dcc56c77deb8791ae25eb4ff
stevenmcdonald <engineers2526@gmail.com>
2019-06-27 12:00:06 -0500

    added the react client side.

--- webapp/server.js
+++ webapp/server.js
@@ -27,5 +27,5 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
